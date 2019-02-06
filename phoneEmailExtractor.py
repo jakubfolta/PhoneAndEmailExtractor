09002a86af15c50b6ac43d9b76b9cc3ca8228c06
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-06 16:45:42 +0100

    continuation

--- phoneEmailExtractor.py
+++ phoneEmailExtractor.py
@@ -3,10 +3,16 @@
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
-(\d{3})|\(\d{3}\)?
-(\s|-|\.)?
-(\d{3})
-(\d{4})
-(\s*(ext|x|ext.)\s*\d{2,5})?
-
+(\d{3})|\(\d{3}\)?              # area code
+(\s|-|\.)?                      # separator
+(\d{3})                         # first 3 digits
+(\s|-|\.)?                      # separator
+(\d{4})                         # last 4 digits
+(\s*(ext|x|ext.)\s*\d{2,5})?    # extension
 )''',re.VERBOSE)
+
+# TODO: Create email regex.
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
