f074007de29645d5e45bf48325b87c74689acf9b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-08 20:41:49 +0100

    finished

--- phoneEmailExtractor.py
+++ phoneEmailExtractor.py
@@ -5,7 +5,7 @@ import pyperclip, re
 # Create phone regex.
 
 phoneRegex = re.compile(r'''(
-(\d{3}|\(\d{3}\))?               # area code
+(\d{3}|\(\d{3}\))?              # area code
 (\s|-|\.)?                      # separator
 (\d{3})                         # first 3 digits
 (\s|-|\.)?                      # separator
@@ -41,40 +41,6 @@ for groups in emailRegex.findall(text):
 if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
     print('Matches copied to clipboard.')
-    print('Matches found: ' + '\n' )
+    print('Matches found: ' + '\n' + '\n'.join(matches))
 else:
     print('No matches found!')
-
-te = input()
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
